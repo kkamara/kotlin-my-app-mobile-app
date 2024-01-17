0bb733604993ba354d1dc54bb06dc86b9b7a295c
kkamara <kamaracomputers@gmail.com>
2024-01-17 15:27:42 +0000

    Add welcome message

--- app/src/main/java/com/kelvinkamara/myapplication/MainActivity.kt
+++ app/src/main/java/com/kelvinkamara/myapplication/MainActivity.kt
@@ -2,10 +2,21 @@ package com.kelvinkamara.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val greetingTextView = findViewById<TextView>(R.id.tvHello)
+        val inputField = findViewById<EditText>(R.id.tvName)
+        val submitButton = findViewById<Button>(R.id.btnSubmit)
+        submitButton.setOnClickListener {
+            val enteredName = inputField.text.toString()
+            val message = "Welcome $enteredName"
+            greetingTextView.text = message
+        }
     }
 }
