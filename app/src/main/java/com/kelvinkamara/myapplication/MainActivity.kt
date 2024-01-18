bc48b3b6cbeb0a4cad23a9ccfff8ac25448951d9
kkamara <kamaracomputers@gmail.com>
2024-01-18 12:11:45 +0000

    Add view offers button that performs screen navigation on click event

--- app/src/main/java/com/kelvinkamara/myapplication/MainActivity.kt
+++ app/src/main/java/com/kelvinkamara/myapplication/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.kelvinkamara.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View.INVISIBLE
+import android.view.View.VISIBLE
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,11 +17,28 @@ class MainActivity : AppCompatActivity() {
         val greetingTextView = findViewById<TextView>(R.id.tvHello)
         val inputField = findViewById<EditText>(R.id.etName)
         val submitButton = findViewById<Button>(R.id.btnSubmit)
+        val offersButton = findViewById<Button>(R.id.btnOffers)
         submitButton.setOnClickListener {
             val enteredName = inputField.text.toString()
-            val message = "Welcome $enteredName"
-            greetingTextView.text = message
-            inputField.text.clear()
+            if (enteredName == "") {
+                offersButton.visibility = INVISIBLE
+                greetingTextView.text = ""
+                Toast.makeText(
+                    this@MainActivity,
+                    "Please enter your name",
+                    Toast.LENGTH_SHORT,
+                ).show()
+            } else {
+                val message = "Welcome $enteredName"
+                greetingTextView.text = message
+                inputField.text.clear()
+                offersButton.visibility = VISIBLE
+            }
+        }
+
+        offersButton.setOnClickListener {
+            val intent  = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
         }
     }
 }
